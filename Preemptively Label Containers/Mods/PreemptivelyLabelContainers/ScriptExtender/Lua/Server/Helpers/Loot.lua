e95663e2ca14f4eaed02683b0bf80bd644a632f3
Atílio <atiliodadalto@protonmail.com>
2024-02-02 13:55:35 -0300

    feat: create `Loot.IsKnockedOut` to be used in `Loot.IsLootable`

--- Preemptively Label Containers/Mods/PreemptivelyLabelContainers/ScriptExtender/Lua/Server/Helpers/Loot.lua
+++ Preemptively Label Containers/Mods/PreemptivelyLabelContainers/ScriptExtender/Lua/Server/Helpers/Loot.lua
@@ -5,8 +5,13 @@ function Loot.IsCorpse(object)
   return GetInventory(object, true, true) ~= nil and Osi.IsDead(object) == 1
 end
 
+function Loot.IsKnockedOut(object)
+  local objectEntityComponents = Ext.Entity.Get(object)
+  return (objectEntityComponents and objectEntityComponents.CanBeLooted and objectEntityComponents.CanBeLooted.Flags ~= 0)
+end
+
 function Loot.IsLootable(object)
-  return (Osi.IsContainer(object) == 1) or Loot.IsCorpse(object)
+  return (Osi.IsContainer(object) == 1) or Loot.IsCorpse(object) or Loot.IsKnockedOut(object)
 end
 
 return Loot
