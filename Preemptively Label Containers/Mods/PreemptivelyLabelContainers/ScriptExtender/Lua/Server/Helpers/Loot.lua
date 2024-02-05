6ee0dfc310d5c72a1a46d5b444ee3889efbeea8f
Atílio <atiliodadalto@protonmail.com>
2024-02-05 20:28:12 -0300

    refactor: don't label buried chests

--- Preemptively Label Containers/Mods/PreemptivelyLabelContainers/ScriptExtender/Lua/Server/Helpers/Loot.lua
+++ Preemptively Label Containers/Mods/PreemptivelyLabelContainers/ScriptExtender/Lua/Server/Helpers/Loot.lua
@@ -10,8 +10,31 @@ function Loot.IsKnockedOut(object)
   return (objectEntityComponents and objectEntityComponents.CanBeLooted and objectEntityComponents.CanBeLooted.Flags ~= 0)
 end
 
+function Loot.IsBuriedChest(object)
+  local objectEntity = Ext.Entity.Get(object)
+  if objectEntity ~= nil then
+    local TemplateName = objectEntity.ServerItem.Template.Name
+    if string.find(TemplateName, "BuriedChest") then
+      return true
+    end
+
+    -- local StatusContainer = objectEntity.StatusContainer
+    local isSurfaceChest = false
+    -- if StatusContainer ~= nil then
+    --   local Statuses = StatusContainer.Statuses
+    --   for _key, Status in pairs(Statuses) do
+    --     if Status == "INSURFACE" then
+    --       isSurfaceChest = true
+    --     end
+    --   end
+
+    return not isSurfaceChest
+  end
+end
+
 function Loot.IsLootable(object)
-  return (Osi.IsContainer(object) == 1) or Loot.IsCorpse(object) or Loot.IsKnockedOut(object)
+  return ((Osi.IsContainer(object) == 1) or Loot.IsCorpse(object) or Loot.IsKnockedOut(object)) and
+      not Loot.IsBuriedChest(object)
 end
 
 return Loot
